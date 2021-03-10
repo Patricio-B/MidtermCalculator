81d16690264c11bb426d47123f49da67e93f7141
mm2288 <mm2288@njit.edu>
2021-03-09 23:29:42 -0500

    Initial commit

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace MidtermCalculator
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("What's up");
         }
     }
 }
