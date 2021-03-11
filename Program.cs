49634325d96d52ec7aae75dab6d6ab1a996e6cb6
mm2288 <mm2288@njit.edu>
2021-03-10 22:32:16 -0500

    Added skeleton console

--- Program.cs
+++ Program.cs
@@ -8,6 +8,35 @@ namespace MidtermCalculator
         {
             //Calculator class instantiation into a calculator object
             Calculator _calculator = new Calculator();
+
+            Console.WriteLine("Welcome to the calculator. You can add, subtract," +
+                "multiply, divide, square, and square-root. Please choose which " +
+                "functionalities you would like by typing them in.\n");
+            Console.WriteLine("'Addition', 'Subtraction', 'Multiplication', " +
+                "'Division', 'Square', 'Square root'\n");
+
+            //user enters operation
+
+            Console.WriteLine("Your calculator has the functionality to: \n");
+
+            Console.WriteLine("Which would you like to do first?\n");
+
+            //user selects operation to use
+
+            Console.WriteLine("Please enter your first number:");
+
+            //user enters num1
+
+            Console.WriteLine("Please enter your second number:");
+
+            //user enters num2
+
+            Console.WriteLine("Result: \n");
+
+            Console.WriteLine("Please select another operation. You could also " +
+                "view history by typing 'history'.\n");
+
+            //user selects operation
         }
     }
 }
