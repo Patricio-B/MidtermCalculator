0391cbbabb837ff3570e110685b1e4750be0a975
mm2288 <mm2288@njit.edu>
2021-03-10 23:01:19 -0500

    Added calculator events

---
+++ Events/CreateCalculationEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Interfaces;
+
+namespace MidtermCalculator.Events
+{
+    public class CreateCalculationEvent
+    {
+        public event EventHandler<CalculationEventArgs> CalculationCompleted;
+
+        public void GrabCalculation(ICalculate calculation)
+        {
+            OnCalculation(calculation);
+        }
+
+        protected virtual void OnCalculation(ICalculate calculation)
+        {
+            CalculationCompleted.Invoke(this, new CalculationEventArgs() { Calculation = calculation });
+        }
+    }
+}
