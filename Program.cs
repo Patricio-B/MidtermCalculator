03cd56482ce7aca75dee5a4f77fd5e27bd6b9088
mm2288 <mm2288@njit.edu>
2021-03-11 00:21:16 -0500

    Fixed console operations

--- Program.cs
+++ Program.cs
@@ -20,9 +20,10 @@ namespace MidtermCalculator
 
             Console.WriteLine("Welcome to the calculator. You can add, subtract," +
                 "multiply, divide, square, and square-root. Please choose which " +
-                "functionalities you would like by typing them in.\n");
-            Console.WriteLine("'Addition', 'Subtraction', 'Multiplication', " +
-                "'Division', 'Square', 'Square root'\n");
+                "functionalities you would like by typing in the number associated " +
+                "with it.\n");
+            Console.WriteLine(" 1. Addition\n 2. Subtraction\n 3. Multiplication" +
+                "\n 4. Division\n 5. Square\n 6. Square Root\n");
 
             //user enters operation
 
