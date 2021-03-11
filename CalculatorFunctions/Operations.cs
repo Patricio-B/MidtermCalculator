1d44940c95aa2e50cf7eab9ac6e21a692c1d4496
Patricio-B <Rumiman2.0@gmail.com>
2021-03-10 20:29:20 -0500

    added lambda operations

---
+++ CalculatorFunctions/Operations.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+//loading the LINQ functionality
+namespace MidtermCalculator.CalculatorFunctions
+{
+    public class Operations
+    {
+        public static Func<double, double, double> Sum = (a, b) => a + b;
+        public static Func<double, double, double> Subtraction = (a, b) => a - b;
+        public static Func<double, double, double> Multiplication = (a, b) => a * b;
+        public static Func<double, double, double> Division = (a, b) => a / b;
+        public static Func<double, double, double> Unassigned = (a, b) => 0;
+
+
+    }
+}
