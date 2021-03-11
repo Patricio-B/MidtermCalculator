54f0a3c345bb047f2aacc1734483565c4ad4c792
mm2288 <mm2288@njit.edu>
2021-03-10 19:55:53 -0500

    Added folders/files

---
+++ Interfaces/ICreate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CalculatorProject.Models;
+
+namespace CalculatorProject.Interfaces
+{
+
+    public interface ICreate
+    {
+        ICalculate Create(List<double> listOfValues, Func<List<double>, double> _operation);
+        ICalculate Create(double a, double b, Func<double, double, double> _operation);
+
+
+    }
+}
+
