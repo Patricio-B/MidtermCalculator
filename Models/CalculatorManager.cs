b3cbcfbf9ecd80ac96bd1ac18f7376e2452469f0
mm2288 <mm2288@njit.edu>
2021-03-17 22:34:30 -0400

    Added decorator

--- Models/CalculatorManager.cs
+++ Models/CalculatorManager.cs
@@ -1,4 +1,4 @@
-﻿/*using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using MidtermCalculator.Publisher;
@@ -8,68 +8,9 @@ namespace MidtermCalculator.Models
 {
     public class CalculatorManager
     {
-        private readonly object lockObj = new object();
-        private Dictionary<Type, IList> subscriber;
-
         public CalculatorManager()
         {
-            subscriber = new Dictionary<Type, IList>();
-        }
-
-        public void Publish<TMessageType>(TMessageType message)
-        {
-            Type t = typeof(TMessageType);
-            IList sublst;
-            if (subscriber.ContainsKey(t))
-            {
-                lock (lockObj)
-                {
-                    sublst = new List<Subscription<TMessageType>>(subscriber[t].Cast<Subscription<TMessageType>>());
-                }
-
-                foreach (Subscription<TMessageType> sub in sublst)
-                {
-                    var action = sub.CreatAction();
-                    if (action != null)
-                        action(message);
-                }
-            }
-        }
-
-        public Subscription<TMessageType> Subscribe<TMessageType>(Action<TMessageType> action)
-        {
-            Type t = typeof(TMessageType);
-            IList actionlst;
-            var actiondetail = new Subscription<TMessageType>(action, this);
-
-            lock (lockObj)
-            {
-                if (!subscriber.TryGetValue(t, out actionlst))
-                {
-                    actionlst = new List<Subscription<TMessageType>>();
-                    actionlst.Add(actiondetail);
-                    subscriber.Add(t, actionlst);
-                }
-                else
-                {
-                    actionlst.Add(actiondetail);
-                }
-            }
-
-            return actiondetail;
-        }
-
-        public void UnSbscribe<TMessageType>(Subscription<TMessageType> subscription)
-        {
-            Type t = typeof(TMessageType);
-            if (subscriber.ContainsKey(t))
-            {
-                lock (lockObj)
-                {
-                    subscriber[t].Remove(subscription);
-                }
-                subscription = null;
-            }
+            //
         }
     }
-}*/
+}
