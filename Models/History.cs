80a041624c0cfd6407e8a92df0edacd1b206f0a6
Patricio-B <Rumiman2.0@gmail.com>
2021-03-18 23:40:54 -0400

    organized history

---
+++ Models/History.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MidtermCalculator.Interfaces;
+
+namespace MidtermCalculator.Models
+{
+{
+    public class History : ICreate
+    {
+        private AddCalculationList _listCalculations = new AddCalculationList();
+
+        public ICalculate Create(double a, double b, Func<double, double, double> _operation)
+        {
+            var _calculation = Calculation.Create(a, b, _operation);
+            _listCalculations.Add(_calculation);
+            return _calculation;
+        }
+
+       // public Calculator() { }
+
+        public List<ICalculate> CreateList()
+        {
+            var result = _listCalculations.Calculations;
+
+            return result;
+
+        }
+    }
+
+}
+}
