dff2db3d7eaa5534b01e8b1a4b7dcbad88ace04f
mm2288 <mm2288@njit.edu>
2021-03-22 20:18:12 -0400

    Added other operations

---
+++ CalculatorFunctions/OtherOperations.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MidtermCalculator.CalculatorFunctions
+{
+    class OtherOperations
+    {
+        public static Func<double, double> Square = (a) => Math.Pow(a, 2);
+        public static Func<double, double> SquareRoot = (a) => Math.Sqrt(a);
+        public static Func<double, double> Unassigned = (a) => 0;
+    }
+}
