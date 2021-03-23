c0c20501ab7c2409369b4713bd855e7def05ef6e
mm2288 <mm2288@njit.edu>
2021-03-23 02:15:18 -0400

    Separated the operation actions

---
+++ CalculatorFunctions/OpDivide.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MidtermCalculator.Interfaces;
+
+namespace MidtermCalculator.CalculatorFunctions
+{
+    class OpDivide : IOperation
+    {
+        public Func<double, double, double> getOperation()
+        {
+            return Operations.Division;
+        }
+    }
+}
---
+++ CalculatorFunctions/OpSum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MidtermCalculator.Interfaces;
+
+namespace MidtermCalculator.CalculatorFunctions
+{
+    public class OpSum : IOperation
+    {
+        public Func<double, double, double> getOperation()
+        {
+            return Operations.Sum;
+        }
+    }
+}
